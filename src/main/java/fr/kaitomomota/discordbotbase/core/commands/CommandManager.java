1020e758e48afdebedad6e7e499c014623d1af3d
KaitoMomota <51933926+KaitoMomota@users.noreply.github.com>
2021-12-30 19:10:35 +0100

    Update CommandManager.java

--- src/main/java/fr/kaitomomota/discordbotbase/core/commands/CommandManager.java
+++ src/main/java/fr/kaitomomota/discordbotbase/core/commands/CommandManager.java
@@ -1,45 +1,32 @@
-package fr.kaitomomota.discordbotbase.core.commands;
+package fr.kaitomomota.discordbotbase.core.commands.finder;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
-import fr.kaitomomota.discordbotbase.DiscordBot;
 import fr.kaitomomota.discordbotbase.core.commands.ICommand;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.User;
+import fr.kaitomomota.discordbotbase.core.commands.impl.fun.HeyCommand;
+import fr.kaitomomota.discordbotbase.core.commands.impl.utils.PrefixCommand;
 
 public class CommandFinder {
 	
-	// Instance
-	public static CommandFinder instance = new CommandFinder();
-			
-	public void findCommandAndExecute(MessageChannel channel, Message message, User user, Member member, Guild guild) {
-		
-		// Parts of command
-		String[] split = message.getContentRaw().split(" ");
-			
-		// Name command String
-		String cmd = split[0].substring(1).toLowerCase();
-			
-		// Arguments
+	private List<ICommand> commands = new ArrayList<>();
 
-		String[] args = new String[0];
-		
-		if (split.length >= 1) {
-			args = Arrays.copyOfRange(split, 1, split.length);
-		}
-		
-		// Search of the command:
-		 
-		ICommand command = DiscordBot.getInstance().getCommandManager().getCommands().stream().filter((commande) -> Arrays.asList(commande.getNames()).stream().map(String::toLowerCase).collect(Collectors.toList()).contains(cmd)).findFirst().orElse(null);
-		if (command == null) { return; }	
-		
-		// Command Execution
-		command.execute(channel, args, user, member, message, guild);
-	
+	public void registerCommands() {
+		// Commands goes here:
+			// FUNS
+				commands.add(new HeyCommand());
+			// UTILS
+				commands.add(new PrefixCommand());
+			// MOD
+
+			// ADMIN
+
+			// NSFW	
+
 	}
 
+	public List<ICommand> getCommands() {
+		return commands;
+	}
+	
 }
